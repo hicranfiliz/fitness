ddfceaaac798ee4d6920c83a5fd0d2d6ff8c87c5
hicranfiliz <h.filiz781@gmail.com>
2023-10-26 20:05:27 +0300

    added activity progress part using barchart

--- lib/view/home/activity_tracker_view.dart
+++ lib/view/home/activity_tracker_view.dart
@@ -1,7 +1,7 @@
 import 'package:fitness/common/colo_extension.dart';
 import 'package:fitness/common_widget/lates_activity_row.dart';
-import 'package:fitness/common_widget/round_button.dart';
 import 'package:fitness/common_widget/today_target_cell.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class ActivityTrackerView extends StatefulWidget {
@@ -12,6 +12,7 @@ class ActivityTrackerView extends StatefulWidget {
 }
 
 class _ActivityTrackerViewState extends State<ActivityTrackerView> {
+  int touchedIndex = -1;
   List latestArr = [
     {
       "image": "assets/img/pic_4.png",
@@ -152,6 +153,155 @@ class _ActivityTrackerViewState extends State<ActivityTrackerView> {
                 ),
               ),
               SizedBox(
+                height: media.width * 0.1,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Activity Progress",
+                    style: TextStyle(
+                        color: TColor.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                    height: 30,
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: TColor.primaryG),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                        items: ["Weekly", "Monthly"]
+                            .map((name) => DropdownMenuItem(
+                                  value: name,
+                                  child: Text(
+                                    name,
+                                    style: TextStyle(
+                                        color: TColor.gray, fontSize: 14),
+                                  ),
+                                ))
+                            .toList(),
+                        onChanged: (value) {},
+                        icon: Icon(
+                          Icons.expand_more,
+                          color: TColor.white,
+                        ),
+                        hint: Text(
+                          "Weekly",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: TColor.white, fontSize: 12),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                  height: media.width * 0.5,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                  decoration: BoxDecoration(
+                      color: TColor.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.black12, blurRadius: 3)
+                      ]),
+                  child: BarChart(BarChartData(
+                    barTouchData: BarTouchData(
+                      touchTooltipData: BarTouchTooltipData(
+                        tooltipBgColor: Colors.grey,
+                        tooltipHorizontalAlignment: FLHorizontalAlignment.right,
+                        tooltipMargin: 10,
+                        getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                          String weekDay;
+                          switch (group.x) {
+                            case 0:
+                              weekDay = 'Monday';
+                              break;
+                            case 1:
+                              weekDay = 'Tuesday';
+                              break;
+                            case 2:
+                              weekDay = 'Wednesday';
+                              break;
+                            case 3:
+                              weekDay = 'Thursday';
+                              break;
+                            case 4:
+                              weekDay = 'Friday';
+                              break;
+                            case 5:
+                              weekDay = 'Saturday';
+                              break;
+                            case 6:
+                              weekDay = 'Sunday';
+                              break;
+                            default:
+                              throw Error();
+                          }
+                          return BarTooltipItem(
+                            '$weekDay\n',
+                            const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: (rod.toY - 1).toString(),
+                                style: TextStyle(
+                                  color: TColor.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                      touchCallback: (FlTouchEvent event, barTouchResponse) {
+                        setState(() {
+                          if (!event.isInterestedForInteractions ||
+                              barTouchResponse == null ||
+                              barTouchResponse.spot == null) {
+                            touchedIndex = -1;
+                            return;
+                          }
+                          touchedIndex =
+                              barTouchResponse.spot!.touchedBarGroupIndex;
+                        });
+                      },
+                    ),
+                    titlesData: FlTitlesData(
+                      show: true,
+                      rightTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      topTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: getTitles,
+                          reservedSize: 38,
+                        ),
+                      ),
+                      leftTitles: const AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: false,
+                        ),
+                      ),
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    barGroups: showingGroups(),
+                    gridData: const FlGridData(show: false),
+                  ))),
+              SizedBox(
                 height: media.width * 0.05,
               ),
               Row(
@@ -193,6 +343,106 @@ class _ActivityTrackerViewState extends State<ActivityTrackerView> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget getTitles(double value, TitleMeta meta) {
+    var style = TextStyle(
+      color: TColor.gray,
+      fontWeight: FontWeight.w500,
+      fontSize: 12,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 0:
+        text = Text('Sun', style: style);
+        break;
+      case 1:
+        text = Text('Mon', style: style);
+        break;
+      case 2:
+        text = Text('Tue', style: style);
+        break;
+      case 3:
+        text = Text('Wed', style: style);
+        break;
+      case 4:
+        text = Text('Thu', style: style);
+        break;
+      case 5:
+        text = Text('Fri', style: style);
+        break;
+      case 6:
+        text = Text('Sat', style: style);
+        break;
+      default:
+        text = Text('', style: style);
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16,
+      child: text,
+    );
+  }
+
+  List<BarChartGroupData> showingGroups() => List.generate(7, (i) {
+        switch (i) {
+          case 0:
+            return makeGroupData(0, 5, TColor.primaryG,
+                isTouched: i == touchedIndex);
+          case 1:
+            return makeGroupData(1, 10.5, TColor.secondaryG,
+                isTouched: i == touchedIndex);
+          case 2:
+            return makeGroupData(2, 5, TColor.primaryG,
+                isTouched: i == touchedIndex);
+          case 3:
+            return makeGroupData(3, 7.5, TColor.secondaryG,
+                isTouched: i == touchedIndex);
+          case 4:
+            return makeGroupData(4, 15, TColor.primaryG,
+                isTouched: i == touchedIndex);
+          case 5:
+            return makeGroupData(5, 11.5, TColor.secondaryG,
+                isTouched: i == touchedIndex);
+          case 6:
+            return makeGroupData(6, 6.5, TColor.primaryG,
+                isTouched: i == touchedIndex);
+          default:
+            return throw Error();
+        }
+      });
+
+  BarChartGroupData makeGroupData(
+    int x,
+    double y,
+    List<Color> barColor, {
+    bool isTouched = false,
+    double width = 22,
+    List<int> showTooltips = const [],
+  }) {
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(
+          toY: isTouched ? y + 1 : y,
+          gradient: LinearGradient(
+              colors: barColor,
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+          width: width,
+          borderSide: isTouched
+              ? const BorderSide(color: Colors.green)
+              : const BorderSide(color: Colors.white, width: 0),
+          backDrawRodData: BackgroundBarChartRodData(
+            show: true,
+            toY: 20,
+            color: TColor.lightGray,
+          ),
+        ),
+      ],
+      showingTooltipIndicators: showTooltips,
     );
   }
 }
